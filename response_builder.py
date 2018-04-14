bc77b9f80a04590bc18aa27df6c89487e76f1e7c
Andre <andre855@gmail.com>
2018-04-14 22:33:56 +0100

    Refactored response_builder to be more modular

--- response_builder.py
+++ response_builder.py
@@ -2,16 +2,50 @@ http_messages = {
     200 : 'OK'
 }
 
-def build_response(status,content,headers = {}):
-    response =  __build_info(status)
-    response += f"Content-Length: {str(len(content))}\r\n"
-
-    for header,value in headers.items():
-        response += f"{header}: {value}\r\n"
-
-    response += "\r\n"
-    response += content
-    return response
-
 def __build_info(status):
     return f"HTTP/1.1 {str(status)} {http_messages[status]}\r\n"
+
+def build_info(status,content):
+    response =  f"HTTP/1.1 {str(status)} {http_messages[status]}\r\n"
+    response += f"Content-Length: {str(len(content))}\r\n"
+    return response
+
+def build_headers(headers):
+    response = ""
+    for key, value in headers.items():
+        response += f"{str(key)}: {str(value)}\r\n"
+    return response
+
+def build_cookies(cookies):
+    response = ""
+    for cookie in cookies:
+        response += f"Set-Cookie: {cookie['name']}={cookie['value']}"
+        
+        if 'expiration' in cookie:
+            response += f"; Expires={cookie['expiration']}"
+
+        if cookie['secure']:
+            response += f"; Secure"
+
+        if cookie['httponly']:
+            response += f"; HttpOnly"
+
+        response += '\r\n'
+
+    return response
+
+def create_cookie(name,value,expiration = None,secure = False,httponly = False):
+    cookie = { 
+        'name'    : name,
+        'value'   : str(value),
+        'secure'  : secure,
+        'httponly': httponly
+    }
+
+    if expiration is not None:
+        cookie['expiration'] = str(expiration)
+        
+    return cookie
+
+def build_body(content):
+    return f"\r\n{str(content)}\r\n"
--- test_response_builder.py
+++ test_response_builder.py
@@ -1,33 +1,69 @@
 import unittest
-from response_builder import build_response
+from response_builder import build_info, build_headers, create_cookie, build_cookies, build_body
 
 class TestResponseBuilder(unittest.TestCase):
-    """Tests for build_response function"""
+    """Tests for response_builder functions"""
     
     def setUp(self):
         self.content = '{ "return": "info" }'
 
-    def test_response_contains_status_code(self):
-        response = build_response(200,self.content)
-        self.assertTrue(response.startswith('HTTP/1.1 200 OK'))
+    def test_build_info(self):
+        response = build_info(200,self.content)
+        expect   = f"HTTP/1.1 200 OK\r\nContent-Length: {str(len(self.content))}\r\n"
+        self.assertEqual(expect,response)
 
     def test_response_contains_content_length(self):
-        response = build_response(200,self.content)
-        response = response.split('\r\n')[1]
-        expected = f"Content-Length: {str(len(self.content))}"
+        headers_to_build = {
+            "Content-Type": "application/json",
+            "Accept-Language": "en-US;en"
+        }       
+        response = build_headers(headers_to_build)
+        expected = "Content-Type: application/json\r\nAccept-Language: en-US;en\r\n"
         self.assertEqual(expected,response)
         
-    def test_response_contains_content(self):
-        response = build_response(200,self.content)
-        self.assertTrue(self.content in response)
+    def test_cookie_creator(self):
+        cookie = create_cookie("test","cookie-value")
+        self.assertEqual('cookie-value',cookie['value'])
+        self.assertEqual(False,cookie['secure'])
+        self.assertEqual(False,cookie['httponly'])
 
-    def test_response_has_separator_from_head_and_body(self):
-        response = build_response(200,self.content)
-        self.assertTrue('\r\n\r\n' in response)
+    def test_build_cookie(self):
+        cookie = create_cookie("test","cookie-value")
+        response = build_cookies([cookie])
+        expected = "Set-Cookie: test=cookie-value\r\n"
+        self.assertEqual(response,expected)
 
-    def test_response_should_contain_headers(self):
-        headers = {'Content-Type': 'application/json'}
-        response = build_response(200,self.content,headers)
-        self.assertTrue('Content-Type: application/json' in response)
-        
+    def test_build_cookie_with_extra_args(self):
+        cookie = create_cookie("test","cookie-value",None,True,True)
+        response = build_cookies([cookie])
+        expected = "Set-Cookie: test=cookie-value; Secure; HttpOnly\r\n"
+        self.assertEqual(response,expected)
+
+    def test_build_response_body(self):
+        expected = f"\r\n{self.content}\r\n"
+        body = build_body(self.content)
+        self.assertEqual(expected,body)
+
+    def test_build_entire_response(self):
+        expected = (
+            'HTTP/1.1 200 OK\r\n'
+            'Content-Length: 20\r\n'
+            'Content-Type: application/json\r\n'
+            'Accept-Language: en-US;en\r\n'
+            'Set-Cookie: test=cookie-value; Secure; HttpOnly\r\n'
+            '\r\n'
+            '{ "return": "info" }\r\n'
+        )
+        response = build_info(200,self.content)
+
+        headers_to_build = {
+            "Content-Type": "application/json",
+            "Accept-Language": "en-US;en"
+        }       
+        response += build_headers(headers_to_build)
+        cookie    = create_cookie("test","cookie-value",None,True,True)
+        response += build_cookies([cookie])
+        response += build_body(self.content)
+        self.assertEqual(expected,response)
+
 unittest.main()
